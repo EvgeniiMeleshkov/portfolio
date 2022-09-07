3764ab033e53315c32ca546819f5e367ed60bac7
Evgenii <emeleshkov@mail.ru>
2022-09-08 02:04:37 +0300

    My oun way in progress

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import s from './Main.module.css'
 import sContainer from '../common/styles/container/Container.module.css'
-import myPhoto from '../assets/IMG_0734.jpeg'
+import myPhoto from '../assets/259FB511-57C1-4200-993F-A67B9457C595_1_201_a.jpeg'
 
 const Main = () => {
     return (
         <div className={s.mainBlock}>
             <div className={`${sContainer.container} ${s.mainContainer}`}>
-            <div className={s.greetings}>
-                    <span>Hi There</span>
+                <div className={s.greetings}>
+                    <p>Hi There</p>
                     <h1>I`m Evgenii Meleshkov</h1>
                     <p>Frontend developer</p>
+                    <h2>
+                        About me...
+                    </h2>
+                    <p>a;odpoaj sdpja poja podjvnpijr pihrt vpiha piha cpiahr vpihjs parihv aspih vars vapiha vpihar pivh asrpivh apishrv </p>
                 </div>
-                    <div className={s.photoFrame}>
 
-                        <img alt='MyPhoto' src={myPhoto} className={s.photo} />
-                    </div>
+                <div className={s.photoFrame}>
+                    <img alt="MyPhoto" src={myPhoto} className={s.photo}/>
+                </div>
             </div>
+
         </div>
     );
 };
