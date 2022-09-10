ea38242b7e1508745ec49a994b7ae00f8ced4928
Evgenii <emeleshkov@mail.ru>
2022-09-10 11:41:14 +0300

    skill css

--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import s from './Skill.module.css'
-import tigerSamurai from '../../assets/1030031.jpg'
 
 type SkillPropsType = {
     title: string
@@ -13,8 +12,11 @@ const Skill = (props: SkillPropsType) => {
             <div className={s.picture} >
                 <img alt={'skill'} src={props.picture}/>
             </div>
-            <h3 className={s.skillTitle}>{props.title}</h3>
-            <p className={s.description}>{props.description}</p>
+            <div className={s.skillTextContainer}>
+                <h3 className={s.skillTitle}>{props.title}</h3>
+                <p className={s.description}>{props.description}</p>
+            </div>
+
         </div>
     );
 };
