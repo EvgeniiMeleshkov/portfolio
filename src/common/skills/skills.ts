297a6651add8afb1b313490c916afd006d2c3aa9
Evgenii <emeleshkov@mail.ru>
2022-09-11 13:05:54 +0300

    projects.ts modified

--- src/common/skills/skills.ts
+++ src/common/skills/skills.ts
@@ -2,15 +2,19 @@ import reactRedux from '../../assets/icones/pngaaa.com-2507964.png'
 import html from '../../assets/icones/HTML.png'
 import MySQL from '../../assets/icones/pnghut_mysql-cluster-database-management-system-mysql-oracle-sql-logo.png'
 import PostgresQL from '../../assets/icones/pnghut_postgresql-relational-database-management-system-elefant.png'
+import axios from '../../assets/icones/pnghut_logo-axios-font-brand-javascript-neyo-stephen-hawking-artificial-intelligence.png'
+import sass from '../../assets/icones/pnghut_responsive-web-design-sass-cascading-style-sheets-sheet-language-magenta-framework.png'
 export const skills = [
     {
         name: 'JS',
-        description: 'JavaScript',
+        description: 'Multi-paradigm programming language. Supports object-oriented, imperative and functional styles.' +
+            ' Is one of the core technologies of the World Wide Web',
         picture: 'https://it-incubator.ru/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FjavaScript.00f974bc.svg&w=1920&q=75'
     },
     {
         name: 'TS',
-        description: 'TypeScript',
+        description: 'A programming language introduced by Microsoft in 2012 and positioned as a web application' +
+            ' development tool that extends the capabilities of JavaScript.',
         picture: 'https://it-incubator.ru/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FtypeScript.d7616c23.svg&w=1920&q=75'
     },
     {
@@ -20,17 +24,36 @@ export const skills = [
     },
     {
         name: 'CSS',
-        description: 'Cascading style sheets',
+        description: 'A Style sheet language used for describing the presentation' +
+            ' of a document written in a markup language such as HTML or XML',
         picture: 'https://it-incubator.ru/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcss.87872113.svg&w=1920&q=75'
     },
     {
+        name: 'SASS',
+        description: 'A CSS-based metalanguage designed to increase the abstraction level of CSS' +
+            ' code and simplify cascading style sheet files.',
+        picture: sass
+    },
+    {
+        name: 'MUI',
+        description: 'A framework that produces ready-made Google solutions for fast and fairly simple web development.',
+        picture: 'https://seeklogo.com/images/M/mui-logo-56F171E991-seeklogo.com.png'
+    },
+    {
+        name: 'Git',
+        description: 'A free and open source distributed version control system designed' +
+            ' to process any project quickly and efficiently.',
+        picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png'
+    },
+    {
         name: 'React',
-        description: 'UI drawing library',
+        description: 'An open source JavaScript library for developing user interfaces. ' +
+            'Can be used to develop single page and mobile applications.',
         picture: 'https://it-incubator.ru/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Freact.2375ceab.svg&w=1920&q=75'
     },
     {
         name: 'Redux',
-        description: 'State management library',
+        description: 'An open source JavaScript library for managing application state.',
         picture: 'https://www.svgrepo.com/show/303557/redux-logo.svg'
     },
     {
@@ -49,23 +72,38 @@ export const skills = [
         picture: 'https://it-incubator.ru/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FrestAPI.1f4206a8.svg&w=1920&q=75'
     },
     {
+        name: 'Axios',
+        description: 'An open source library that allows you to make HTTP requests.',
+        picture: axios
+    },
+    {
+        name: 'Postman',
+        description: 'An API platform that allows developers to design, build, test, and iterate their APIs.',
+        picture: 'https://www.svgrepo.com/show/354202/postman-icon.svg'
+    },
+    {
         name: 'Story book',
-        description: 'Testing & visualizing tool',
+        description: 'A UI library that can be used to document components. It also allows you to organize and assemble components.',
         picture: 'https://it-incubator.ru/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FstoryBook.0cbf5baa.svg&w=1920&q=75'
     },
     {
+        name: 'Snapshot Testing',
+        description: 'compare previous screenshot with the actual screenshot that is taken during the test run.',
+        picture: 'https://www.svgrepo.com/show/353930/jest.svg'
+    },
+    {
         name: 'Unit tests',
-        description: 'App unit tests',
+        description: 'The process of programming, checking the correctness of individual modules of the source code of programs.',
         picture: 'https://it-incubator.ru/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FunitTest.ecdada0b.svg&w=1920&q=75'
     },
     {
         name: 'MySQL',
-        description: 'MySQL data-base',
+        description: 'MySQL relative data-base',
         picture: MySQL
     },
     {
         name: 'PostgresQL',
-        description: 'PostgresQL data-base',
+        description: 'PostgresQL relative data-base',
         picture: PostgresQL
     },
 ]
