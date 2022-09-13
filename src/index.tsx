dfd9e80bbe1f105c32b9627404a4e3aed103475b
Evgenii <emeleshkov@mail.ru>
2022-09-13 03:27:18 +0300

    fonts...

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from 'react-router-dom';
+import './fonts/Oxanium/Oxanium-VariableFont_wght.ttf'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
