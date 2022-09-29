77e5b864a7ccbc6f95bf328fa96932244e4f11d0
Evgenii <emeleshkov@mail.ru>
2022-09-29 10:38:58 +0300

    AboutMe.tsx modified

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react';
 import s from './Main.module.css'
 import sContainer from '../common/styles/container/Container.module.css'
 import myPhoto from '../assets/IMG_2503.jpg'
-import AboutMe from '../aboutMe/AboutMe';
 import {NavLink} from 'react-router-dom';
 
 const Main = () => {
