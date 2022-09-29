dcc7525f49024417cb06f0a5e351eba42a9428a9
Evgenii <emeleshkov@mail.ru>
2022-09-29 10:43:11 +0300

    skills.ts modified

--- src/common/skills/skills.ts
+++ src/common/skills/skills.ts
@@ -19,7 +19,7 @@ export const skills = [
     },
     {
         name: 'HTML',
-        description: 'How to meet ladies',
+        description: 'Hypertext Markup Language, a standardized system for tagging text files to achieve font, colour, graphic, and hyperlink effects on World Wide Web pages.',
         picture: html
     },
     {
